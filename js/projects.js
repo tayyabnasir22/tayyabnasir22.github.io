64c3d8f06ccad9d220e2f9ef765c2da1ca8e0607
tayyabnasir22 <tayyabnasir22@gmail.com>
2025-10-18 17:35:57 +0800

    bug fix

--- js/projects.js
+++ js/projects.js
@@ -3,7 +3,7 @@ fetch('data/projects.json')
   .then(res => res.json())
   .then(data => {
     const container = document.getElementById('projectsContainer');
-    container.innerHTML = data.map(d => `
+    container.innerHTML += data.map(d => `
         <div class="d-flex flex-column flex-md-row justify-content-between mb-2">
             <div class="flex-grow-1">
                 <h3 class="mb-0"><a href="${d.url + d.details}">${d.name}</a></h3>
