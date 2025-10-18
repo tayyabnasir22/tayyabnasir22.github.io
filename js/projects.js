97bde0df8d5f6112dadc595c1aca3a565792b363
tayyabnasir22 <tayyabnasir22@gmail.com>
2025-10-18 17:21:51 +0800

    bug fix

--- js/projects.js
+++ js/projects.js
@@ -3,14 +3,14 @@ fetch('data/projects.json')
   .then(res => res.json())
   .then(data => {
     const container = document.getElementById('projectsContainer');
-    container.innerHTML += `
+    container.innerHTML = navData.data.map(data => `
         <div class="d-flex flex-column flex-md-row justify-content-between mb-2">
             <div class="flex-grow-1">
-                <h3 class="mb-0"><a href="${projectDetailsLink + element.details}">${element.name}</a></h3>
-                <div class="subheading mb-2">${element.company}</div>
+                <h3 class="mb-0"><a href="${data.details}">${data.name}</a></h3>
+                <div class="subheading mb-2">${data.company}</div>
             </div>
-            <div class="flex-shrink-0 mb-5"><span class="text-primary">${element.year}</span></div>
+            <div class="flex-shrink-0 mb-5"><span class="text-primary">${data.year}</span></div>
         </div><hr/>
-    `;
+    `).join('');
   })
   .catch(err => console.error('Error loading awards.json:', err));
