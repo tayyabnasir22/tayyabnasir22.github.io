b3a422519ebd1448edaa07649eea3293dc8e5d8a
tayyabnasir22 <tayyabnasir22@gmail.com>
2025-10-18 17:26:20 +0800

    bug fix

--- js/projects.js
+++ js/projects.js
@@ -3,10 +3,10 @@ fetch('data/projects.json')
   .then(res => res.json())
   .then(data => {
     const container = document.getElementById('projectsContainer');
-    container.innerHTML = navData.data.map(data => `
+    container.innerHTML = data.map(data => `
         <div class="d-flex flex-column flex-md-row justify-content-between mb-2">
             <div class="flex-grow-1">
-                <h3 class="mb-0"><a href="${data.details}">${data.name}</a></h3>
+                <h3 class="mb-0"><a href="${data.url + data.details}">${data.name}</a></h3>
                 <div class="subheading mb-2">${data.company}</div>
             </div>
             <div class="flex-shrink-0 mb-5"><span class="text-primary">${data.year}</span></div>
