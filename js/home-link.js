6618bcb976d7f260480268d0ef41b33b766c8f9e
tayyabnasir22 <tayyabnasir22@gmail.com>
2026-02-09 16:14:57 +0800

    added toptal link + github contrib

--- js/home-link.js
+++ js/home-link.js
@@ -6,11 +6,16 @@ fetch('data/home-link.json')
   })
   .then(navData => {
     const navbarContainer = document.getElementById('navbarLinks');
-    navbarContainer.innerHTML = navData.links.map(link => `
+    navbarContainer.innerHTML = navData.links.map((link, index) => {
+    const isLast = index === navData.links.length - 1;
+    const linkClass = isLast ? 'nav-link special-class' : 'nav-link js-scroll-trigger';
+    const liner = isLast ? '<hr class="bolder-line"/>' : ''
+    return `
       <li class="nav-item">
-        <a class="nav-link js-scroll-trigger" href="${link.href}">${link.label}</a>
+        ${liner}
+        <a class="${linkClass}" href="${link.href}">${link.label}</a>
       </li>
-    `).join('');
+    `}).join('');
   })
   .catch(error => {
     console.error('Error loading navbar.json:', error);
